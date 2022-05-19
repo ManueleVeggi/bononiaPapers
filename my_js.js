e8d0a01dee8c49b274fd812ef5158d8db1fb4fd9
ManueleVeggi <manuele.veggi@studio.unibo.it>
2022-05-19 14:00:02 +0200

    tables

---
+++ my_js.js
@@ -0,0 +1,8 @@
+function show(nr) {
+    document.getElementById("table1").style.display="none";
+    document.getElementById("table2").style.display="none";
+    document.getElementById("table3").style.display="none";
+    document.getElementById("table4").style.display="none";
+    document.getElementById("table5").style.display="none";
+    document.getElementById("table"+nr).style.display="block";
+}
